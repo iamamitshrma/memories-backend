df36c585edc0fe9932bfe0bf5bcb9a758190ee2e
Amit Sharma <iamamitshrma@gmail.com>
2022-04-05 14:31:46 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+
+import postRoutes from './routes/posts.js';
+import userRoutes from './routes/users.js';
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+app.use('/posts', postRoutes);
+app.use('/user', userRoutes);
+
+//heroku
+app.get('/', (req, res) => {
+    res.send('App is Running');
+})
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONNECTION_URL,  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => app.listen(PORT, () => console.log(`Server Running on port: ${PORT}`)))
+.catch((err) => console.log(err.message));
